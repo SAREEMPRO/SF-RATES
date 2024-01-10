11dda5a8be1ff23aced2f77c1c4abef6dd9749b3
SAREEMPRO <Mdsareem1@gmail.com>
2024-01-10 20:38:41 +0530

    ss

--- index.js
+++ index.js
@@ -68,6 +68,8 @@
 
 //   convert();
 // });
+
+
 const exchangeRates = { usd: 1 };
 const fromCurrency = document.querySelector(".converter-container #from");
 const toCurrency = document.querySelector(".converter-container #to");
@@ -75,47 +77,52 @@ const inputAmount = document.querySelector(".converter-container .input-amount")
 const result = document.querySelector(".converter-container .result");
 const swapBtn = document.querySelector(".converter-container .swap-btn");
 
-const addCurrencyOption = (currencyCode, container) => {
-  const currencyInfo = exchangeRates[currencyCode];
-  const { code, name } = currencyInfo;
-
-  const option = document.createElement("option");
-  option.value = code;
-  option.textContent = `${code} - ${name}`;
-
-  container.appendChild(option);
-};
-
 const init = async () => {
   try {
-    const res = await fetch("http://www.floatrates.com/daily/usd.json");
+    const res = await fetch("https://open.er-api.com/v6/latest");
     const data = await res.json();
 
     if (res.ok) {
-      for (const currencyCode in data) {
-        exchangeRates[currencyCode] = data[currencyCode].rate;
-        addCurrencyOption(currencyCode, fromCurrency);
-        addCurrencyOption(currencyCode, toCurrency);
+      for (const currencyCode in data.rates) {
+        const rate = data.rates[currencyCode];
+        const option1 = createOption(currencyCode, rate);
+        const option2 = createOption(currencyCode, rate);
+
+        fromCurrency.appendChild(option1);
+        toCurrency.appendChild(option2);
+
+        exchangeRates[currencyCode] = rate;
       }
 
       toCurrency.value = toCurrency.options[1].value;
       convert();
     }
   } catch (error) {
-    console.log("Error loading currency data");
+    console.log("Error loading currency data", error);
   }
+};
+
+const createOption = (code, rate) => {
+  const option = document.createElement("option");
+  option.value = code;
+  option.textContent = `${code} - ${rate}`;
+  return option;
 };
 
 init();
 
 const convert = () => {
   const inputValue = parseFloat(inputAmount.value);
-  const fromCurrencyValue = fromCurrency.value.toLowerCase();
-  const toCurrencyValue = toCurrency.value.toLowerCase();
+  const fromCurrencyValue = fromCurrency.value;
+  const toCurrencyValue = toCurrency.value;
 
-  const convertedValue = (inputValue * exchangeRates[toCurrencyValue]) / exchangeRates[fromCurrencyValue];
+  const convertedValue =
+    (inputValue * exchangeRates[toCurrencyValue]) /
+    exchangeRates[fromCurrencyValue];
 
-  const resultValue = `<span class='result-currency'>${toCurrencyValue}</span> ${convertedValue.toFixed(2)}`;
+  const resultValue = `<span class='result-currency'>${toCurrencyValue}</span> ${convertedValue.toFixed(
+    2
+  )}`;
 
   result.innerHTML = isNaN(convertedValue) ? "Invalid Input" : resultValue;
 };
